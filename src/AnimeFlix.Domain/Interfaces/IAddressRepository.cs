54df47f315dd748b41fc0708a11d5ca98a95428c
williamfelipecosc <will25felipe@gmail.com>
2023-10-06 17:27:22 -0300

    feat: address repository interface

---
+++ src/AnimeFlix.Domain/Interfaces/IAddressRepository.cs
@@ -0,0 +1,10 @@
+﻿using AnimeFlix.Domain.Core.Data;
+using AnimeFlix.Domain.Models.Address;
+
+namespace AnimeFlix.Domain.Interfaces
+{
+    public interface IAddressRepository : IRepository<AddressModel>
+    {
+        Task<AddressModel> GetById(int id);
+    }
+}
